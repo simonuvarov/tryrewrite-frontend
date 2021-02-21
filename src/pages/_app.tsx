d8f80d8e3144d236ffba44c5149631a7d01f7fb9
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-02-21 14:27:32 +0300

    remove fathom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,8 @@
-import { load, trackPageview } from 'fathom-client';
 import type { AppProps /*, AppContext */ } from 'next/app';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import DefaultSeo from '../components/DefaultSeo';
 import '../css/tailwind.css';
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    load('FTQRLWBI', {
-      includedDomains: ['wriby.com']
-    });
-
-    function onRouteChangeComplete() {
-      trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, []);
-
   return (
     <>
       <DefaultSeo />
