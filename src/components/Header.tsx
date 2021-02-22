42bd052efd0382251f8086f2af1421c29a2c447b
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-02-22 18:17:51 +0300

    remove about link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,11 +24,6 @@ export default function Header({ className = '' }: HeaderProps): ReactElement {
               Blog
             </a>
           </Link>
-          <Link href="#/about">
-            <a className="ml-2 inline-flex justify-center items-center px-4 py-2 rounded text-gray-500 hover:bg-gray-200 text-center font-medium ">
-              About
-            </a>
-          </Link>
         </div>
       </nav>
     </header>
