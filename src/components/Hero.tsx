f26177d22f711c2febbdb029ccf37427306526d5
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-02-22 11:10:31 +0300

    wip: improve hero layout on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
   return (
-    <div className="bg-gray-100 w-full pt-20 pb-44 px-2">
+    <div className="bg-gray-100 w-full pt-20 pb-44 px-4">
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight text-gray-800 w-full">
+        <h1 className="text-3xl md:text-7xl font-extrabold tracking-tight text-gray-800 w-full">
           Improve your&nbsp;writing in&nbsp;a&nbsp;smart way
         </h1>
-        <p className="text-2xl mt-6 text-gray-600 max-w-3xl">
+        <p className="text-lg md:text-2xl mt-6 text-gray-600 max-w-3xl">
           Find new topics, generate new ideas, keep track of time, check
           grammar, and get instant feedback – all in one place.
         </p>
