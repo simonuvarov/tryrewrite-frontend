17a6d392ac44e8351938345197835a3901a7d2cb
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-02-23 18:27:57 +0300

    remove email form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import FeatureGrid from '../components/FeatureGrid';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
-import WaitListForm from '../components/WaitListForm';
 
 export function Home() {
   return (
@@ -11,11 +10,6 @@ export function Home() {
       <Hero />
       <div className="md:px-4">
         <FeatureGrid className="w-full md:max-w-5xl mx-auto -mt-20" />
-      </div>
-      <div className="w-full px-2 mt-8 md:mt-32 md:mb-10">
-        <div className="max-w-3xl mx-auto">
-          <WaitListForm />
-        </div>
       </div>
       <Footer />
     </>
