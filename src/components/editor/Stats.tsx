952ecf0da2e20a3c2bc24a5bb063d863fdb711cb
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-03-28 18:48:03 +0300

    add bands

--- src/components/editor/Stats.tsx
+++ src/components/editor/Stats.tsx
@@ -7,25 +7,12 @@ export const Stats = (props: StatsProps) => {
 
   return (
     <div>
-      <div>
-        <h2 className="font-medium text-lg">Task Achivement</h2>
-        {props.data.ta.map((ruleResult: any) => (
-          <p className="mt-2" key={ruleResult.name}>
-            {ruleResult.message} – {ruleResult.band}
-          </p>
-        ))}
-      </div>
-      <div>
-        <h2 className="font-medium text-lg mt-6">Coherence and Cohesion</h2>
-        {props.data.cc.map((ruleResult: any) => (
-          <p className="mt-2" key={ruleResult.name}>
-            {ruleResult.message} – {ruleResult.band}
-          </p>
-        ))}
-      </div>
-      <div>
-        <h2 className="font-medium text-lg mt-6">Lexical Resource</h2>
-        {props.data.lr.map((ruleResult: any) => (
+      <h1 className="text-2xl font-semibold">Band: {props.data.band}</h1>
+      <div className="mt-5">
+        <h2 className="font-medium text-lg">
+          Task Achivement: {props.data.ta.band}
+        </h2>
+        {props.data.ta.results.map((ruleResult: any) => (
           <p className="mt-2" key={ruleResult.name}>
             {ruleResult.message} – {ruleResult.band}
           </p>
@@ -33,9 +20,29 @@ export const Stats = (props: StatsProps) => {
       </div>
       <div>
         <h2 className="font-medium text-lg mt-6">
-          Grammatical Range and Accuracy
+          Coherence and Cohesion: {props.data.cc.band}
         </h2>
-        {props.data.gr.map((ruleResult: any) => (
+        {props.data.cc.results.map((ruleResult: any) => (
+          <p className="mt-2" key={ruleResult.name}>
+            {ruleResult.message} – {ruleResult.band}
+          </p>
+        ))}
+      </div>
+      <div>
+        <h2 className="font-medium text-lg mt-6">
+          Lexical Resource: {props.data.lr.band}
+        </h2>
+        {props.data.lr.results.map((ruleResult: any) => (
+          <p className="mt-2" key={ruleResult.name}>
+            {ruleResult.message} – {ruleResult.band}
+          </p>
+        ))}
+      </div>
+      <div>
+        <h2 className="font-medium text-lg mt-6">
+          Grammatical Range and Accuracy: {props.data.gr.band}
+        </h2>
+        {props.data.gr.results.map((ruleResult: any) => (
           <p className="mt-2" key={ruleResult.name}>
             {ruleResult.message} – {ruleResult.band}
           </p>
