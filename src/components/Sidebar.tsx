71f26b146f9202e49def91fdbf3bc90b4d2d76ef
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-15 18:50:04 +0300

    make sidebar take issues from store

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,50 +1,16 @@
 import { useBandsStore } from '../stores/useBandScore';
+import { useIssuesStore } from '../stores/useIssuesStore';
 import { OverallBand } from './Band';
 import { IssueCard } from './IssueCard';
 
-interface StatsProps {
-  data?: any;
-}
-
-const issues = [
-  {
-    offset: 241,
-    length: 8,
-    message:
-      'The pronoun ‘I’ must be used with a non-third-person form of a verb: “support”',
-    shortMessage: 'Some short message',
-    affects: 'Grammatical Range and Accuracy',
-    isInline: true
-  },
-  {
-    affects: 'Task Achievement',
-    message: 'You are required to write a conclusion in this type of essay.',
-    shortMessage: 'Missing conclusion',
-    isInline: false
-  },
-  {
-    affects: 'Task Achievement',
-    message: 'You need to write at least 250 words',
-    shortMessage: 'Word count',
-    isInline: false
-  },
-  {
-    message:
-      "Generally it's recommended to use 4 to 5 paragraphs in your essay",
-    shortMessage: 'Paragraph count',
-    isInline: false,
-    affects: 'Coherence and Cohesion'
-  }
-];
-
-export const Sidebar = (props: StatsProps) => {
+export const Sidebar = () => {
   const { bands } = useBandsStore();
-
+  const { issues } = useIssuesStore();
   return (
     <div>
       <OverallBand score={bands.overall} />
       {issues.map(i => (
-        <IssueCard issue={i} id={i.shortMessage} />
+        <IssueCard issue={i} />
       ))}
     </div>
   );
