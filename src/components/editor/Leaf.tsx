b99123dfa3ef2dd353efbcfb10b4f5522496462d
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-17 17:04:26 +0300

    remove background from issues

--- src/components/editor/Leaf.tsx
+++ src/components/editor/Leaf.tsx
@@ -5,34 +5,25 @@ export const Leaf = ({ children, leaf, attributes }: RenderLeafProps) => {
   switch (leaf.affects) {
     case CRITERIA_TYPE.LR:
       return (
-        <span {...attributes} className="bg-red-100 border-b-2 border-red-400 ">
+        <span {...attributes} className="border-b-2 border-red-400">
           {children}
         </span>
       );
     case CRITERIA_TYPE.CC:
       return (
-        <span
-          {...attributes}
-          className="bg-blue-100 border-b-2  border-blue-400"
-        >
+        <span {...attributes} className="border-b-2  border-blue-400">
           {children}
         </span>
       );
     case CRITERIA_TYPE.TA:
       return (
-        <span
-          {...attributes}
-          className="bg-green-100 border-b-2  border-green-400"
-        >
+        <span {...attributes} className="order-b-2  border-green-400">
           {children}
         </span>
       );
     case CRITERIA_TYPE.GR:
       return (
-        <span
-          {...attributes}
-          className="bg-yellow-100 border-b-2  border-yellow-400"
-        >
+        <span {...attributes} className="border-b-2  border-yellow-400">
           {children}
         </span>
       );
