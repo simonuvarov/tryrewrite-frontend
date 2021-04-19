f4c1b60d95d2005ba0409869d5def866cfcb4241
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-19 17:35:05 +0300

    add simple loading state for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,28 @@ import { IssueCard } from './IssueCard';
 export const Sidebar = () => {
   const { issues, bands } = useGraderResultStore();
 
-  if (!issues || !bands) return <p>Loading...</p>;
+  if (!issues || !bands)
+    return (
+      <div className="w-full">
+        <div className="animate-pulse flex flex-col space-y-8">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-200 rounded"></div>
+            <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+          </div>
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-200 rounded"></div>
+            <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+          </div>
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-200 rounded"></div>
+            <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <div className="w-full">
