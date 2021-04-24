49791b34fafe3e61870c44af593342a246472344
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-24 19:36:55 +0300

    add band label

--- src/components/PaperCard.tsx
+++ src/components/PaperCard.tsx
@@ -2,6 +2,18 @@ import moment from 'moment';
 import Link from 'next/link';
 import { Paper } from '../services/paper.service';
 
+interface BandLabelProps {
+  score: number;
+}
+
+export const BandLabel = (props: BandLabelProps) => {
+  return (
+    <div className="inline px-2 py-1 text-xs font-semibold rounded bg-gray-100 text-gray-600">
+      BAND {parseFloat(props.score.toString()).toFixed(1)}
+    </div>
+  );
+};
+
 interface PaperCardProps {
   paper: Paper;
 }
@@ -11,7 +23,8 @@ export const PaperCard = (props: PaperCardProps) => {
     <Link href={`/paper/${props.paper.id}`}>
       <li key={props.paper.id}>
         <div className="max-w-lg w-full bg-white rounded-lg shadow px-12 py-8 cursor-pointer">
-          <h2 className="w-auto font-semibold text-lg line-clamp-2 text-gray-900">
+          <BandLabel score={props.paper.overallBand} />
+          <h2 className="w-auto font-semibold text-lg line-clamp-2 text-gray-900 mt-3">
             {props.paper.question}
           </h2>
           <p className="w-auto line-clamp-2 text-gray-700 leading-6 mt-1">
