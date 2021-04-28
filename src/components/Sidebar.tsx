54b5a0e29da9c885e9be8d66864640825ad49a8a
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-28 13:28:40 +0300

    improve sidebar loading state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,14 +7,12 @@ export const Sidebar = () => {
   if (!issues || !bands)
     return (
       <div className="w-full">
-        <div className="flex justify-center items-center ">
-          <span className="text-lg text-medium text-gray-500">
-            Overall Band:
-          </span>
-          <span className="ml-3 text-3xl font-bold text-gray-800">0.0</span>
+        <div className="flex flex-col justify-center items-center py-6">
+          <span className="text-xl text-medium text-gray-500">Overall</span>
+          <span className="animate-pulse  ">...</span>
         </div>
-        <ul className="animate-pulse flex flex-col space-y-8 mt-5">
-          <li className="px-12 py-8 border border-gray-100 shadow-sm rounded-lg">
+        <ul className="animate-pulse flex flex-col space-y-8 mt-5 px-12">
+          <li className="px-12 py-4">
             <div className="h-5 bg-gray-200 rounded w-3/4"></div>
             <div className="mt-4 space-y-1">
               <h3 className="h-5 bg-gray-200 rounded w-3/4"></h3>
@@ -23,7 +21,7 @@ export const Sidebar = () => {
               <p className=" w-64 h-5 bg-gray-200 rounded"></p>
             </div>
           </li>
-          <li className="px-12 py-8 border border-gray-100 shadow-sm rounded-lg">
+          <li className="px-12 py-4">
             <div className="h-5 bg-gray-200 rounded w-3/4"></div>
             <div className="mt-4 space-y-1">
               <h3 className="h-5 bg-gray-200 rounded w-3/4"></h3>
@@ -32,7 +30,7 @@ export const Sidebar = () => {
               <p className=" w-64 h-5 bg-gray-200 rounded"></p>
             </div>
           </li>
-          <li className="px-12 py-8 border border-gray-100 shadow-sm rounded-lg">
+          <li className="px-12 py-4">
             <div className="h-5 bg-gray-200 rounded w-3/4"></div>
             <div className="mt-4 space-y-1">
               <h3 className="h-5 bg-gray-200 rounded w-3/4"></h3>
@@ -49,7 +47,7 @@ export const Sidebar = () => {
     <div className="">
       <div className="flex flex-col justify-center items-center py-6">
         <span className="text-xl text-medium text-gray-500">Overall</span>
-        <span className="ml-3 font-bold text-gray-600 text-7xl">
+        <span className="font-bold text-gray-600 text-7xl">
           {parseFloat(bands.overall.toString()).toFixed(1)}
         </span>
       </div>
