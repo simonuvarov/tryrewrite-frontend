af959f409b163a088150b8cb43fe717f988b8b21
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-29 12:16:28 +0300

    add red error ring to inputs

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -13,6 +13,7 @@ interface FormInputProps {
   onBlur: (event: React.FormEvent) => void;
 }
 export const FormInput = (props: FormInputProps) => {
+  const shouldShowError = props.error && props.touched;
   return (
     <div>
       <label
@@ -23,7 +24,11 @@ export const FormInput = (props: FormInputProps) => {
       </label>
       <input
         id={props.label.toLowerCase()}
-        className="block w-full px-4 py-2 border border-gray-200 rounded-md text-base leading-8 text-gray-700 outline-none focus:outline-none focus:ring transition-shadows transition mt-2"
+        className={`block w-full px-4 py-2 border border-gray-200 rounded-md text-base leading-8 text-gray-700 outline-none focus:outline-none focus:ring ${
+          shouldShowError
+            ? 'focus:ring-red-300 border-red-400'
+            : 'focus:border-blue-400'
+        } transition-shadows transition mt-2`}
         value={props.value}
         onChange={props.onChange}
         autoFocus={props.autofocus || false}
@@ -31,7 +36,7 @@ export const FormInput = (props: FormInputProps) => {
         type={props.type || 'text'}
         spellCheck={props.spellcheck || false}
       />
-      {props.error && props.touched && (
+      {shouldShowError && (
         <label
           htmlFor={props.label.toLowerCase()}
           className="block text-red-600 text-sm ml-1 mt-1"
