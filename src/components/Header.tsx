9d6912c2ccaced62ca40d0b567677732282a8009
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-30 11:49:43 +0300

    refactor/redesign main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { ChevronRightIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 import React, { ReactElement } from 'react';
 import { Logo } from '../components/Logo';
 
@@ -8,9 +10,16 @@ interface HeaderProps {
 export default function Header({ className = '' }: HeaderProps): ReactElement {
   return (
     <header className={`w-full pt-8 pb-4 px-4 ${className}`}>
-      <nav className="flex justify-between md:mx-auto max-w-3xl">
+      <nav className="flex justify-between md:mx-auto max-w-5xl">
         <Logo className="px-2" />
-        <div className="flex justify-end items-center"></div>
+        <div className="flex justify-end items-center">
+          <Link href="/signin">
+            <a className="flex items-center pl-4 pr-3 py-2 leading-4 bg-gray-100 hover:bg-gray-200 rounded-full text-sm font-medium text-gray-600 hover:text-gray-700 transition-colors outline-none">
+              <span>Sign in</span>
+              <ChevronRightIcon className="h-5 w-5 ml-0.5" />
+            </a>
+          </Link>
+        </div>
       </nav>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
-import FeatureGrid from '../components/FeatureGrid';
+import Link from 'next/link';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import Hero from '../components/Hero';
 
 export function Home() {
   return (
     <>
-      <Header className="bg-gray-100" />
-      <Hero />
-      <div className="md:px-4">
-        <FeatureGrid className="w-full md:max-w-5xl mx-auto -mt-20" />
-      </div>
+      <Header className="bg-white" />
+      <section className="bg-white w-full pt-20 pb-44 px-4 flex flex-col items-center">
+        <h1 className="text-3xl md:text-7xl font-extrabold tracking-tight text-gray-800 max-w-xl text-center ">
+          Write. Check. Correct. Repeat.
+        </h1>
+        <p className="text-lg md:text-2xl mt-6 text-gray-500 max-w-3xl text-center ">
+          Get instant feedback and track your progress – all in one place
+        </p>
+        <Link href="/signup">
+          <a
+            className={`mt-10 px-4 py-2 bg-blue-600 hover:bg-blue-500 text-white rounded-md font-medium text-base leading-8 transition outline-none `}
+          >
+            Get started now
+          </a>
+        </Link>
+      </section>
+
       <Footer />
     </>
   );
