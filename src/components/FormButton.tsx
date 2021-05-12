78d5ebbfa19a9e971d2dd3c5081bf133dd39e2d1
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-05-12 18:10:48 +0300

    remove outline from button

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -8,9 +8,10 @@ interface FormButtonProps {
 export const FormButton = (props: FormButtonProps) => {
   return (
     <button
-      className={`px-4 py-2 bg-blue-600 hover:bg-blue-500 text-white rounded-md text-base leading-8 font-medium transition ${
+      className={`px-4 py-2 bg-blue-600 hover:bg-blue-500 text-white rounded-md text-base leading-8 font-medium transition focus:outline-none  ${
         props.className || ''
       }`}
+      type="submit"
     >
       {props.children}
     </button>
