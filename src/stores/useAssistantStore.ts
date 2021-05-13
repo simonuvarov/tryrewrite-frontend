883d0e4f5fefac6aba4d6fbdd8c89a9cfee4f765
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-05-13 15:19:32 +0300

    remove unused functions from store

--- src/stores/useAssistantStore.ts
+++ src/stores/useAssistantStore.ts
@@ -3,8 +3,6 @@ import create, { State } from 'zustand';
 interface AssistantStoreProps extends State {
   isShowing: boolean;
   toggleShowing: () => void;
-  show: () => void;
-  hide: () => void;
   selected: string | null;
   select: (id: string) => void;
 }
@@ -13,7 +11,5 @@ export const useAssistantStore = create<AssistantStoreProps>((set, get) => ({
   isShowing: false,
   selected: null,
   toggleShowing: () => set({ isShowing: !get().isShowing }),
-  show: () => set({ isShowing: true }),
-  hide: () => set({ isShowing: false }),
   select: (id: string) => set({ selected: id })
 }));
