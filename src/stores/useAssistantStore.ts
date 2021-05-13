75e7268646e373437753c5ed62cfe1fefe455349
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-05-13 15:20:07 +0300

    change null -> undefined

--- src/stores/useAssistantStore.ts
+++ src/stores/useAssistantStore.ts
@@ -3,13 +3,13 @@ import create, { State } from 'zustand';
 interface AssistantStoreProps extends State {
   isShowing: boolean;
   toggleShowing: () => void;
-  selected: string | null;
+  selected?: string;
   select: (id: string) => void;
 }
 
 export const useAssistantStore = create<AssistantStoreProps>((set, get) => ({
   isShowing: false,
-  selected: null,
+  selected: undefined,
   toggleShowing: () => set({ isShowing: !get().isShowing }),
   select: (id: string) => set({ selected: id })
 }));
