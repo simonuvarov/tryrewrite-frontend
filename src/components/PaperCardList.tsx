766a8b7c4f5285213cf302551134a4c74140f916
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-16 18:34:23 +0300

    increase font sizes on dashboard

--- src/components/PaperCardList.tsx
+++ src/components/PaperCardList.tsx
@@ -16,18 +16,6 @@ const PaperCardSkeleton = () => {
   );
 };
 
-interface BandLabelProps {
-  score: number;
-}
-
-const BandLabel = (props: BandLabelProps) => {
-  return (
-    <div className="inline px-2 py-1 text-xs font-semibold rounded bg-gray-100 text-gray-600">
-      BAND {parseFloat(props.score.toString()).toFixed(1)}
-    </div>
-  );
-};
-
 interface PaperCardProps {
   paper: Paper;
 }
@@ -35,15 +23,19 @@ interface PaperCardProps {
 const PaperCard = (props: PaperCardProps) => {
   return (
     <Link href={`/paper/${props.paper.id}`}>
-      <li className="hover:bg-gray-50 hover:cursor-pointer flex justify-between items-center">
-        <div className="flex flex-col flex-shrink-0 py-4 px-2 max-w-lg w-full">
-          <div className="font-medium truncate text-gray-800">
+      <li className="hover:bg-gray-50 hover:cursor-pointer flex justify-between items-center p-4">
+        <div id="left" className="flex flex-col flex-shrink-0 max-w-lg w-full">
+          <div className="font-medium text-lg truncate text-gray-800">
             {props.paper.question}
           </div>
-          <div className="truncate mt-2 text-gray-600">{props.paper.body}</div>
+          <div className="truncate text-lg mt-2 text-gray-600">
+            {props.paper.body}
+          </div>
         </div>
-        <div className="flex font-medium text-gray-800 px-4">
-          {parseFloat(props.paper.overallBand.toString()).toFixed(1)}
+        <div id="right">
+          <div className="flex font-medium text-xl text-gray-700 px-4">
+            {parseFloat(props.paper.overallBand.toString()).toFixed(1)}
+          </div>
         </div>
       </li>
     </Link>
