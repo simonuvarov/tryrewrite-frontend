d253fcbb95957b083384a84d77b96ce07470dd5b
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-16 18:01:13 +0300

    remove unused files

--- src/components/Section.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from 'react';
-
-const Section: FunctionComponent = props => {
-  return <article className="w-full mb-24">{props.children}</article>;
-};
-
-export default Section;
