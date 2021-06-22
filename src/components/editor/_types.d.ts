0ef778a7c9e57e27fa12c5e03063955bca21fcdc
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-22 09:58:39 +0300

    add slate editor types

---
+++ src/components/editor/_types.d.ts
@@ -0,0 +1,9 @@
+import { BaseEditor } from 'slate';
+import { ReactEditor } from 'slate-react';
+
+// https://docs.slatejs.org/concepts/12-typescript
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+  }
+}
