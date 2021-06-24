19ac330fe16fdefbc8816dfd3c292ff077e43197
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-24 19:33:23 +0300

    fix banner text color

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ interface BannerProps {
 
 export const Banner = (props: BannerProps) => {
   return (
-    <article className="flex bg-blue-50 border-b border-blue-100 items-center justify-center text-sm leading-5 font-medium py-3 text-blue-900 px-2">
+    <article className="flex bg-blue-50 border-b border-blue-100 items-center justify-center text-sm leading-5 font-medium py-3 text-blue-700 px-2">
       {props.children}
     </article>
   );
