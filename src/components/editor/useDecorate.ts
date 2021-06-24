1910b5bfa1d1092d7ceb11073ddd7ab46d99df7a
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-24 13:21:18 +0300

    finish refactoring useDecorate

--- src/components/editor/useDecorate.ts
+++ src/components/editor/useDecorate.ts
@@ -6,22 +6,40 @@ import {
 } from '../../services/paper.service';
 import { useAssistantStore } from '../../stores/useAssistantStore';
 import { usePaperStore } from '../../stores/usePaperStore';
-import { splitTextIntoParagraphRanges } from './splitTextIntoParagraphRanges';
 
 export interface IssueRange extends BaseRange {
   id: string;
   affects: CRITERIA_TYPE;
 }
 
-export const isPartOfParagraph = (
-  issue: Issue,
-  paragraphRange: [number, number]
-) => {
+export const splitTextIntoParagraphRanges = (text: string) => {
+  const paragraphRanges: Array<[number, number]> = [];
+  let offset = 0;
+  text.split('\n').map((p: string) => {
+    const start = offset;
+    const end = offset + p.length + '\n'.length;
+    paragraphRanges.push([start, end]);
+    offset = offset + p.length + '\n'.length;
+  });
+
+  return paragraphRanges;
+};
+
+const isPartOfParagraph = (issue: Issue, paragraphRange: [number, number]) => {
   return (
     issue.isInline &&
     issue.offset < paragraphRange[1] &&
     issue.offset >= paragraphRange[0]
   );
+};
+
+const extractParagraphIssues = (
+  issues: Array<Issue>,
+  paragraphRange: [number, number]
+) => {
+  return issues.filter(h =>
+    isPartOfParagraph(h, paragraphRange)
+  ) as Array<InlineIssue>;
 };
 
 export const useDecorate = () => {
@@ -39,9 +57,9 @@ export const useDecorate = () => {
       splitTextIntoParagraphRanges(paper.body);
     const currentTextRange = paragraphRanges[path[0]];
 
-    for (const issue of issues.filter(h => {
-      return isPartOfParagraph(h, currentTextRange);
-    }) as Array<InlineIssue>) {
+    const relatedIssues = extractParagraphIssues(issues, currentTextRange);
+
+    for (const issue of relatedIssues) {
       const length = issue.length;
       const start = issue.offset - currentTextRange[0];
       const end = start + length;
