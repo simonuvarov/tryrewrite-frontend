cce2672a08aa69875cd0e5003fd9a03ab83cf934
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-29 17:31:14 +0300

    add title to sign(in/up) pages

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
 import React, { useEffect } from 'react';
@@ -101,26 +102,29 @@ function Signup() {
   if (user) return null;
 
   return (
-    <div className="flex bg-gray-50 min-h-screen flex-col justify-center">
-      <div className="bg-white px-20 py-16 rounded-xl mx-auto shadow-md border border-gray-200 w-full max-w-xl">
-        <div className="w-auto">
-          <h2 className="text-left text-2xl font-semibold text-gray-700">
-            Create your Rewrite account
-          </h2>
-        </div>
-        <div className="w-full flex flex-col mx-auto max-w-md mt-8">
-          <SignupForm redirectTo="/dashboard" />
-        </div>
-        <div className="mt-6 text-sm ">
-          <span className="text-gray-500 ">Have an account?</span>
-          <Link href="/signin">
-            <a className="ml-2 font-medium text-blue-600 hover:text-blue-500 transition-colors">
-              Sign in
-            </a>
-          </Link>
+    <>
+      <NextSeo title="Create your Rewrite account" />
+      <div className="flex bg-gray-50 min-h-screen flex-col justify-center">
+        <div className="bg-white px-20 py-16 rounded-xl mx-auto shadow-md border border-gray-200 w-full max-w-xl">
+          <div className="w-auto">
+            <h2 className="text-left text-2xl font-semibold text-gray-700">
+              Create your Rewrite account
+            </h2>
+          </div>
+          <div className="w-full flex flex-col mx-auto max-w-md mt-8">
+            <SignupForm redirectTo="/dashboard" />
+          </div>
+          <div className="mt-6 text-sm ">
+            <span className="text-gray-500 ">Have an account?</span>
+            <Link href="/signin">
+              <a className="ml-2 font-medium text-blue-600 hover:text-blue-500 transition-colors">
+                Sign in
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
