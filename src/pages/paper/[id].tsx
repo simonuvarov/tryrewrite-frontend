ba40161ef0c9c3e8aa01221bb44841fb337ac12d
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-29 15:16:52 +0300

    add back button

--- src/pages/paper/[id].tsx
+++ src/pages/paper/[id].tsx
@@ -1,3 +1,4 @@
+import { ArrowLeftIcon } from '@heroicons/react/solid';
 import { useRouter } from 'next/dist/client/router';
 import React, { useEffect } from 'react';
 import { AssistantButton } from '../../components/AssistantButton';
@@ -20,6 +21,13 @@ export function Edit() {
 
   return (
     <div className="h-screen">
+      <button
+        className="absolute flex items-center top-7 left-8 text-gray-400 font-medium pl-4 pr-5 py-2 hover:bg-gray-100 rounded transition-colors duration-300 focus:outline-none"
+        onClick={() => router.back()}
+      >
+        <ArrowLeftIcon className="h-5 w-5 mr-1" />
+        Back
+      </button>
       <EditorProvider paperId={id as string}>
         <AssistantButton className="absolute right-4 top-4" />
         <div className="flex min-h-full">
