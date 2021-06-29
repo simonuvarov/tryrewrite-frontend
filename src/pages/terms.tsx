bc9591348f706245a6b45f8e75fa38535e685bd4
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-29 17:28:46 +0300

    add terms

---
+++ src/pages/terms.tsx
@@ -0,0 +1,147 @@
+import { NextSeo } from 'next-seo';
+
+const Terms = () => {
+  return (
+    <>
+      <NextSeo title="Terms of Service" />
+      <article className="prose lg:prose-xl mx-auto py-40 px-4">
+        <h1>Terms of Service</h1>
+        <p>We may update these Terms of Service in the future.</p>
+        <p>
+          Our guiding principle is to collect only what we need. Here’s what
+          that means in practice. When you use our Services, now or in the
+          future, you are agreeing to the latest Terms of Service. That's true
+          for any of our existing and future products and all features that we
+          add to our Services over time. There may be times where we do not
+          exercise or enforce any right or provision of the Terms of Service; in
+          doing so, we are not waiving that right or provision. These terms do
+          contain a limitation of our liability.
+        </p>
+        <p>
+          If you violate any of the terms, we may terminate your account. That's
+          a broad statement and it means you need to place a lot of trust in us.
+        </p>
+        <p>
+          By accessing and using tryrewrite.com (the Service) and any other
+          site, application, API or embedded content owned or operated by us,
+          you accept and agree to be bound by the following terms and
+          conditions:
+        </p>
+        <h2>Account Terms</h2>
+        <ol>
+          <li>
+            You are responsible for maintaining the security of your account and
+            password. The Company cannot and will not be liable for any loss or
+            damage from your failure to comply with this security obligation.
+          </li>
+          <li>
+            You may not use the Services for any purpose outlined in our Use
+            Restrictions policy.
+          </li>
+          <li>
+            You are responsible for all content posted and activity that occurs
+            under your account. That includes content posted by others who
+            either: (a) have access to your login credentials; or (b) have their
+            own logins under your account.
+          </li>
+        </ol>
+        <h2>Payment, Refunds, and Plan Changes</h2>
+        <ol>
+          <li>
+            If you are using a free version of one of our Services, it is really
+            free: we do not ask you for your credit card and — just like for
+            customers who pay for our Services — we do not sell your data.
+          </li>
+          <li>
+            If you are upgrading from a free plan to a paid plan, we will charge
+            your card immediately and your billing cycle starts on the day of
+            upgrade. For other upgrades or downgrades in plan level, the new
+            rate starts from the next billing cycle.
+          </li>
+          <li>
+            We issue refunds for digital products within 14 days of the original
+            purchase of the product.
+          </li>
+        </ol>
+
+        <h2>Cancellation and Termination</h2>
+        <ol>
+          <li>
+            You are solely responsible for properly canceling your account.
+          </li>
+          <li>
+            All of your content will be inaccessible from the Services
+            immediately upon account cancellation. Within 30 days, all content
+            will be permanently deleted from active systems and logs. Within 60
+            days, all content will be permanently deleted from our backups. We
+            cannot recover this information once it has been permanently
+            deleted.
+          </li>
+          <li>
+            We have the right to suspend or terminate your account and refuse
+            any and all current or future use of our Services for any reason at
+            any time. Suspension means you and any other users on your account
+            will not be able to access the account or any content in the
+            account. Termination will furthermore result in the deletion of your
+            account or your access to your account, and the forfeiture and
+            relinquishment of all content in your account. We also reserve the
+            right to refuse the use of the Services to anyone for any reason at
+            any time. We have this clause because statistically speaking, out of
+            the hundreds of thousands of accounts on our Services, there is at
+            least one doing something nefarious. There are some things we
+            staunchly stand against and this clause is how we exercise that
+            stance. For more details, see our Use Restrictions policy.
+          </li>
+          <li>
+            Verbal, physical, written or other abuse (including threats of abuse
+            or retribution) of Company employee or officer will result in
+            immediate account termination.
+          </li>
+        </ol>
+        <h2>Uptime, Security, and Privacy</h2>
+        <ol>
+          <li>
+            Your use of the Services is at your sole risk. We provide these
+            Services on an “as is” and “as available” basis.{' '}
+          </li>
+          <li>
+            We reserve the right to temporarily disable your account if your
+            usage significantly exceeds the average usage of other customers of
+            the Services. Of course, we'll reach out to the account owner before
+            taking any action except in rare cases where the level of use may
+            negatively impact the performance of the Service for other
+            customers.
+          </li>
+          <li>
+            We take many measures to protect and secure your data through
+            backups, redundancies, and encryption. We enforce encryption for
+            data transmission from the public Internet.
+          </li>
+          <li>
+            When you use our Services, you entrust us with your data. We take
+            that trust to heart. You agree that Basecamp may process your data
+            as described in our Privacy Policy and for no other purpose.
+          </li>
+          <li>
+            We use third party vendors and hosting partners to provide the
+            necessary hardware, software, networking, storage, and related
+            technology required to run the Services.
+          </li>
+        </ol>
+        <h2>Copyright and Content Ownership</h2>
+        <p>
+          You agree that we own all of the intellectual property rights in the
+          Service. You grant us a non-exclusive, royalty-free, irrevocable,
+          perpetual and sub-licensable right to use, reproduce, distribute and
+          display (including for commercial purposes) on the Service and in
+          other media any content or material that you post on the Service, and
+          any name(s) and/or avatar under which you post such content. Other
+          than this right, we claim no intellectual property rights in relation
+          to the information or content that you upload onto the Service.
+        </p>
+      </article>
+    </>
+  );
+};
+
+export default Terms;
